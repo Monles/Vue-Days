541748e9c792f34d2be43873b1c38869207e7e69
monles <iceueb@gmail.com>
2022-07-18 13:08:54 +0100

    vue day

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-Vue.createApp({
+const vm = Vue.createApp({
   data() {
     return {
       firstName: 'John',
+      middleName: '',
       lastName: 'Doe',
+      url: 'https://google.com',
+      raw_url: '<a href="https://google.com" target="_blank">Google</a>',
+      age: 20,
     };
+  },
+  methods: {
+    increment() {
+      this.age++;
+    },
+    updateLastName(msg, event) {
+      console.log(msg);
+      this.lastName = event.target.value;
+    },
+    updateMiddleName(event) {
+      this.middleName = event.target.value;
+    },
+  },
+  computed: {
+    fullName() {
+      console.log('Full name computed property was called!');
+
+      this.age;
+
+      return `${this.firstName} ${
+        this.middleName
+      } ${this.lastName.toUpperCase()}`;
+    },
   },
 }).mount('#app');
-
-Vue.createApp({
-  data() {
-    return {
-      firstName: 'Jane',
-      lastName: 'Doe',
-    };
-  },
-}).mount('#app2');
